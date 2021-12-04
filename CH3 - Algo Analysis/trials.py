e8d3230cf69a451e95dfefaedffaf162dcc5b340
Daquiver <Cabrokwa11@gmail.com>
2021-12-04 19:44:25 +0000

    Initial commit

---
+++ CH3 - Algo Analysis/trials.py
@@ -0,0 +1,8 @@
+
+# A data structure is a systematic way or organizing and accessing data.
+# An algorithmis a step-by-step procedure to perform a task in finite time.
+from time import time
+start_time = time()
+# do something
+end_time = time()
+elapsed = end_time - start_time
