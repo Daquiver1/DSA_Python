94073ac5dd8ad4720aae75bddfeb55b70aacf8f4
Christian Abrokwa <Cabrokwa11@gmail.com>
2021-12-30 10:34:37 +0000

    Did some reading on it

---
+++ CH10 - Maps, Hash Tables and Skip Lists/maps1.py
@@ -0,0 +1,13 @@
+# Maps are similar to dictionaries. So just think dictionary. 
+
+# The Map ADT
+
+"""
+M[k]: Returns the value associated with key k in map M.
+M[k] =v: Associates value v with key k in map M.
+del M[k]: Removes from map M the item with key equal to K
+len(M): Returns the number of items in map M.
+iter(M): The default iteration for a map gernerates a sequence of keys
+			in the map. In python this is implemented with __iter__ and it
+			allows loops of the form for k in M.
+"""
